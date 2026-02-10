916e8abb28670fa4decf96a1ae54f9b24d47d5a9
AllenWn <allenwei0503@gmail.com>
2026-02-09 23:32:28 -0600

    config update

--- pipeline/config.py
+++ pipeline/config.py
@@ -2,22 +2,23 @@
 Cocofluo pipeline configuration.
 """
 
-N_CHANNELS = 4  # Number of channels in the image
-DAPI_CH = 1  # Structural channel index (1-based)
+N_CHANNELS = 13  # Number of channels in the image
+DAPI_CH = 5  # Structural channel index (1-based)
 
 CHANNEL_NAMES = {
-    1: "dapi",
-    2: "cas",
-    3: "fos",
-    4: "glp1r",
-    5: "",
-    6: "",
-    7: "",
-    8: "",
-    9: "",
-    10: "",
-    11: "",
-    12: "",
+    1: "adcyap1",
+    2: "bdhf",
+    3: "casr",
+    4: "cck",
+    5: "dapi",
+    6: "dbh",
+    7: "fos",
+    8: "gaba",
+    9: "glp1r",
+    10: "htr2c",
+    11: "htr3a",
+    12: "tac1",
+    13: "vglut2",
 }
 
 DAPI_ENHANCEMENT = True  # Apply contrast stretch (1–99 percentile normalization)
@@ -28,12 +29,12 @@ CELLPROB_THRESHOLD = 0.0
 
 MARKER_EXTRACT_PERCENTILE = 99.8  # Top (100-P)% pixels = positive;
 
-MARKER_EXPRESS_THRESHOLD = 0.07  # ratio of marker-positive area / cell area
+MARKER_EXPRESS_THRESHOLD = 0.08  # ratio of marker-positive area / cell area
 
 COLOCALIZATION_GROUPS = [
-    (2, 4),
-    (3, 4),
-    (2, 3, 4),
+    (3, 7),
+    (7, 9),
+    (3, 7, 9),
 ]
 
 CELL_SEG_SAMPLING = True  # Save cell segmentation QC images
